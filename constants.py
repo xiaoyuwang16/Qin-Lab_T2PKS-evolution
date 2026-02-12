47c0a0c68dc3d7f94e77ab7d7e7eee634f55424c
xiaoyuwang16 <xiaoyuwang.bnu@gmail.com>
2026-02-12 19:18:56 +0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -5,12 +5,9 @@ WINDOW_SIZES = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100, 110,
 
 # Color scheme for visualization
 COLOR_SCHEME = {
-    'TypeII/III': '#FF6B6B',
-    'FormIvariants': '#4ECDC4',  
-    'Anaero': '#FFBE0B',
-    'FormIAB/CD': '#9B5DE5',
-    'Thermus': '#00F5D4',
-    'Other': '#808080'
+    'FABF': '#FF6B6B',
+    'KSa': '#4ECDC4',  
+    'KSb': '#FFBE0B'
 }
 
 # KNN Graph parameters
